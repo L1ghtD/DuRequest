9e8a3a90dedcd75b58ddedb05a3df3776f5a22b0
jfDu <jeffduuu@gmail.com>
2023-12-15 22:39:02 +0800

    添加全局拦截器

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -7,9 +7,25 @@ class DuRequest {
   // request 实例 => axios 实例
   constructor(config: AxiosRequestConfig) {
     this.instance = axios.create(config)
+
+    this.instance.interceptors.request.use(config =>{
+      console.log("全局请求成功拦截器");
+      return config
+    }, err => {
+      console.log("全局请求失败拦截器");
+      return err
+    })
+
+    this.instance.interceptors.response.use(config => {
+      console.log("全局响应成功拦截器");
+      return config
+    }, err => {
+      console.log("全局响应失败拦截器");
+      return err
+    })
   }
 
-  request(config: AxiosRequestConfig) {
+  request(config: AxiosRequestConfig ) {
     return this.instance.request(config)
   }
 
